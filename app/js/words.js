a24d6c851b89f28806e9b3b81f05340f71e0ab8c
Gabor Szabo <gabor@szabgab.com>
2012-12-24 17:52:39 +0200

    switching between pages

--- app/js/words.js
+++ app/js/words.js
@@ -1,32 +1,39 @@
-var add_word_button = document.getElementById('add_words_button');
+var add_words_button = document.getElementById('add_words_button');
 var select_lesson_button = document.getElementById('select_lesson_button');
 
-var main_page = document.getElementById('main_page');
+var home_page = document.getElementById('home_page');
 var select_lesson_page = document.getElementById('select_lesson_page');
 
-function add_word() {
+function add_words() {
    alert("Not yet implemented");
 }
 
 function hide_pages() {
-    main_page.classList.remove('current_page');
+    home_page.classList.remove('current_page');
     select_lesson_page.classList.remove('current_page');
+    add_words_page.classList.remove('current_page');
+};
+
+function switch_to(page) {
+   hide_pages();
+   page.classList.add('current_page');
 };
 
 
-add_word_button.addEventListener( 'click', add_word );
+add_words_button.addEventListener( 'click',function() {
+    switch_to(add_words_page);
+});
+
 select_lesson_button.addEventListener( 'click', function() {
-   hide_pages();
-   select_lesson_page.classList.add('current_page');
+    switch_to(select_lesson_page);
 });
 
 var home_buttons = document.getElementsByClassName('home_button');
 for (var i=0; i < home_buttons.length; i++) {
    home_buttons[i].addEventListener( 'click', function() {
-      hide_pages();
-      main_page.classList.add('current_page');
+      switch_to(home_page);
    });
 }
 
 
-main_page.classList.add('current_page');
+home_page.classList.add('current_page');
