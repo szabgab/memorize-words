5d07caf0d1bc1d8cd59871e7501ff5b8b680eee6
Gabor Szabo <gabor@szabgab.com>
2012-12-24 17:47:12 +0200

    add 2 more pages

--- app/js/words.js
+++ app/js/words.js
@@ -1,8 +1,32 @@
 var add_word_button = document.getElementById('add_words_button');
+var select_lesson_button = document.getElementById('select_lesson_button');
+
+var main_page = document.getElementById('main_page');
+var select_lesson_page = document.getElementById('select_lesson_page');
 
 function add_word() {
    alert("Not yet implemented");
 }
 
+function hide_pages() {
+    main_page.classList.remove('current_page');
+    select_lesson_page.classList.remove('current_page');
+};
 
-add_word_button.addEventListener( "click", add_word );
+
+add_word_button.addEventListener( 'click', add_word );
+select_lesson_button.addEventListener( 'click', function() {
+   hide_pages();
+   select_lesson_page.classList.add('current_page');
+});
+
+var home_buttons = document.getElementsByClassName('home_button');
+for (var i=0; i < home_buttons.length; i++) {
+   home_buttons[i].addEventListener( 'click', function() {
+      hide_pages();
+      main_page.classList.add('current_page');
+   });
+}
+
+
+main_page.classList.add('current_page');
